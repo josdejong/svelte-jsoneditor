9fb9618a1760d8fcd82f796a6d39022cc3d334c1
Jos de Jong <wjosdejong@gmail.com>
2021-11-03 09:43:46 +0100

    fix: trigger emitting a change when replacing something

--- src/lib/components/modes/treemode/TreeMode.svelte
+++ src/lib/components/modes/treemode/TreeMode.svelte
@@ -201,7 +201,7 @@
       activeItem
     )
 
-    patch(operations, newSelection)
+    handlePatch(operations, newSelection)
 
     await tick()
 
@@ -218,7 +218,7 @@
       replacementText
     )
 
-    patch(operations, newSelection)
+    handlePatch(operations, newSelection)
 
     await tick()
 
