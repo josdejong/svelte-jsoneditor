4f6bf6aaa10170a9342f43575f558413528c4133
Jos de Jong <wjosdejong@gmail.com>
2022-05-27 11:35:40 +0200

    fix: export classes correctly in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,24 +1,21 @@
-import _JSONEditor from './components/JSONEditor.svelte'
-import _SortModal from './components/modals/SortModal.svelte'
-import _TransformModal from './components/modals/TransformModal.svelte'
-import _BooleanToggle from './plugins/value/components/BooleanToggle.svelte'
-import _ColorPicker from './plugins/value/components/ColorPicker.svelte'
-import _EditableValue from './plugins/value/components/EditableValue.svelte'
-import _EnumValue from './plugins/value/components/EnumValue.svelte'
-import _ReadonlyValue from './plugins/value/components/ReadonlyValue.svelte'
-import _TimestampTag from './plugins/value/components/TimestampTag.svelte'
+import JSONEditor from './components/JSONEditor.svelte'
+import SortModal from './components/modals/SortModal.svelte'
+import TransformModal from './components/modals/TransformModal.svelte'
+import BooleanToggle from './plugins/value/components/BooleanToggle.svelte'
+import ColorPicker from './plugins/value/components/ColorPicker.svelte'
+import EditableValue from './plugins/value/components/EditableValue.svelte'
+import EnumValue from './plugins/value/components/EnumValue.svelte'
+import ReadonlyValue from './plugins/value/components/ReadonlyValue.svelte'
+import TimestampTag from './plugins/value/components/TimestampTag.svelte'
 
-export const JSONEditor = _JSONEditor
+// editor
+export { JSONEditor }
+export * from './types'
 
 // value plugins
 export { renderValue } from './plugins/value/renderValue.js'
 export { renderJSONSchemaEnum } from './plugins/value/renderJSONSchemaEnum.js'
-export const BooleanToggle = _BooleanToggle
-export const ColorPicker = _ColorPicker
-export const EditableValue = _EditableValue
-export const EnumValue = _EnumValue
-export const ReadonlyValue = _ReadonlyValue
-export const TimestampTag = _TimestampTag
+export { BooleanToggle, ColorPicker, EditableValue, EnumValue, ReadonlyValue, TimestampTag }
 
 // validator plugins
 export { createAjvValidator } from './plugins/validator/createAjvValidator.js'
@@ -29,8 +26,7 @@ export { javascriptQueryLanguage } from './plugins/query/javascriptQueryLanguage
 export { jmespathQueryLanguage } from './plugins/query/jmespathQueryLanguage.js'
 
 // utils
-export const SortModal = _SortModal
-export const TransformModal = _TransformModal
+export { SortModal, TransformModal }
 export { getJSONSchemaOptions, findSchema, findEnum } from './utils/jsonSchemaUtils.js'
 export { parseJSONPointerWithArrayIndices } from './utils/jsonPointer.js'
 export { isTextContent, isLargeContent, estimateSerializedSize } from './utils/jsonUtils.js'
