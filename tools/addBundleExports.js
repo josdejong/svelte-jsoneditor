5cb352f4fcac697c81af5ab19e17a9b88b6be589
Jos de Jong <wjosdejong@gmail.com>
2022-05-27 11:37:00 +0200

    fix: create a shortcut file dist/jsoneditor.d.ts linking to the main index.d.ts file

--- tools/addBundleExports.js
+++ tools/addBundleExports.js
@@ -1,19 +1,27 @@
 import { readFileSync, writeFileSync } from 'fs'
 import path from 'path'
 
-export function addBundleExports(packageFolder, file, sourcemapFile) {
+export function addBundleExports(packageFolder, file, sourcemapFile, definitionsFile) {
   const pkgFile = path.join(packageFolder, 'package.json')
 
   const distPkg = JSON.parse(String(readFileSync(pkgFile)))
   const relativeFile = normalizePath(packageFolder, file)
   const relativeSourcemapFile = normalizePath(packageFolder, sourcemapFile)
+  const relativeDefinitionsFile = normalizePath(packageFolder, definitionsFile)
+
+  console.info('add bundle exports', {
+    relativeFile,
+    relativeSourcemapFile,
+    relativeDefinitionsFile
+  })
 
   const updatedDistPkg = {
     ...distPkg,
     exports: {
       ...distPkg.exports,
       [relativeFile]: relativeFile,
-      [relativeSourcemapFile]: relativeSourcemapFile
+      [relativeSourcemapFile]: relativeSourcemapFile,
+      [relativeDefinitionsFile]: relativeDefinitionsFile
     }
   }
 
