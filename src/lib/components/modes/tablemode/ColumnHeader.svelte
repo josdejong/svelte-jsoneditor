7f3cbaeec43a93b8f34f4059090a2c8c79dde2fe
Jos de Jong <wjosdejong@gmail.com>
2022-11-17 15:25:46 +0100

    fix: limit the number of characters of a column header

--- src/lib/components/modes/tablemode/ColumnHeader.svelte
+++ src/lib/components/modes/tablemode/ColumnHeader.svelte
@@ -7,11 +7,16 @@
   import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons'
   import { isEmpty, isEqual } from 'lodash-es'
   import { SORT_DIRECTION_NAMES } from '../../../constants.ts'
+  import { truncate } from '$lib/utils/stringUtils.js'
+  import { MAX_HEADER_NAME_CHARACTERS } from '$lib/constants.js'
 
   export let path: JSONPath
   export let sortedColumn: SortedColumn | undefined
   export let readOnly: boolean
   export let onSort: (sortedColumn: SortedColumn) => void
+
+  // TODO: improve truncating of long column names when they are a deeply nested path: the last item from the path should be visible, and halfway the path is least interesting
+  $: columnName = !isEmpty(path) ? stripRootObject(stringifyJSONPath(path)) : 'values'
 
   $: sortDirection = isEqual(path, sortedColumn?.path) ? sortedColumn.sortDirection : undefined
   $: sortDirectionName = SORT_DIRECTION_NAMES[sortDirection]
@@ -33,10 +38,10 @@
   class="jse-column-header"
   class:jse-readonly={readOnly}
   on:click={handleSort}
-  title={!readOnly ? 'Sort the data on this column' : 'Column name'}
+  title={!readOnly ? columnName + ' (Click to sort the data by this column)' : columnName}
 >
   <span class="jse-column-name">
-    {!isEmpty(path) ? stripRootObject(stringifyJSONPath(path)) : 'values'}
+    {truncate(columnName, MAX_HEADER_NAME_CHARACTERS)}
   </span>
   {#if sortDirection !== undefined}
     <span class="jse-column-sort-icon" title={`Currently sorted in ${sortDirectionName} order`}>
