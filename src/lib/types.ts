410fd801297faa46afef201569bfd79792de17e1
Jos de Jong <wjosdejong@gmail.com>
2022-12-12 13:54:53 +0100

    fix: #96 add missing properties to `JSONEditorPropsOptional`

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,7 @@
 import type { JSONPatchDocument, JSONPath, JSONPointer, JSONValue } from 'immutable-json-patch'
 import type { SvelteComponentTyped } from 'svelte'
 import type { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+import { parseJSONPath, stringifyJSONPath } from "$lib/utils/pathUtils";
 
 export type { JSONValue, JSONPointer, JSONPath, JSONPatchDocument } from 'immutable-json-patch'
 
@@ -421,7 +422,11 @@ export interface JSONEditorPropsOptional {
   statusBar?: boolean
   escapeControlCharacters?: boolean
   escapeUnicodeCharacters?: boolean
-  validator?: Validator
+  flattenColumns: true
+  parser: JSONParser
+  validator: Validator | null
+  validationParser: JSONParser
+  pathParser: JSONPathParser
 
   queryLanguages?: QueryLanguage[]
   queryLanguageId?: string
