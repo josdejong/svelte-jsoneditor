b9b61607603d19ea48a6b223bd6a5a36ee86bace
Jos de Jong <wjosdejong@gmail.com>
2023-08-21 13:47:10 +0200

    chore: assert the extracted dependencies when creating package.json

--- tools/createVanillaPackageJson.js
+++ tools/createVanillaPackageJson.js
@@ -1,6 +1,7 @@
 // create package.json and copy files like LICENSE.md to package-vanilla
 
 import path from 'path'
+import assert from 'assert'
 import { readFileSync, writeFileSync } from 'fs'
 import { getAbsolutePath } from './utils/getAbsolutePath.mjs'
 import { generateExports } from './utils/generateExports.mjs'
@@ -10,14 +11,30 @@ const vanillaPackageFolder = getAbsolutePath(import.meta.url, '..', 'package-van
 const exports = generateExports(vanillaPackageFolder)
 
 const pkg = JSON.parse(String(readFileSync(getAbsolutePath(import.meta.url, '..', 'package.json'))))
-const types = String(
-  readFileSync(getAbsolutePath(import.meta.url, '..', 'package-vanilla', 'index.d.ts'))
-)
+const typesPath = getAbsolutePath(import.meta.url, '..', 'package-vanilla', 'index.d.ts')
+const types = String(readFileSync(typesPath))
 
 // scan the index.d.ts bundle file for all occurrences of "import { ...} from '...'" and extract the name
-const usedDependencyNames = Array.from(types.matchAll(/(import|export) .+ from '(.+)'/g)).map(
-  (match) => match[2]
+const usedDependencyNames = uniq(
+  Array.from(types.matchAll(/(import|export) .+ from '(.+)'/g))
+    .map((match) => match[2])
+    .sort()
 )
+const expectedDependencyNames = [
+  '@fortawesome/free-solid-svg-icons',
+  'ajv',
+  'immutable-json-patch',
+  'svelte'
+]
+
+// We do not want to get surprises
+assert.deepStrictEqual(
+  usedDependencyNames,
+  expectedDependencyNames,
+  `Used dependencies found in "${typesPath}" does not equal the expected dependencies. ` +
+    'Please update the list in createVanillaPackageJson.js manually.'
+)
+
 const usedDependencies = usedDependencyNames.reduce((deps, name) => {
   deps[name] = pkg.dependencies[name]
   return deps
@@ -43,3 +60,7 @@ writeFileSync(
   path.join(vanillaPackageFolder, 'package.json'),
   JSON.stringify(vanillaPackage, null, 2)
 )
+
+function uniq(array) {
+  return [...new Set(array)]
+}
