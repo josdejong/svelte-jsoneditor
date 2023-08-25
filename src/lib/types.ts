42979fffa4ab37c80e2fcff0c314b0cf5942c608
Vincenzo Scamporlino <me@vinzscam.dev>
2023-08-25 09:56:29 +0200

    fix: export missing types
    Signed-off-by: Vincenzo Scamporlino <me@vinzscam.dev>

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,7 @@
 import type { JSONPatchDocument, JSONPath, JSONValue } from 'immutable-json-patch'
 import type { SvelteComponent } from 'svelte'
 import type { IconDefinition } from '@fortawesome/free-solid-svg-icons'
+export type { JSONValue, JSONPath }
 
 export type TextContent = { text: string } | { json: undefined; text: string }
 
@@ -112,7 +113,7 @@ export interface TextSelection {
 
 export type JSONEditorSelection = JSONSelection | TextSelection
 
-type JSONPointer = string // Would like to use "import type { JSONPointer } from 'immutable-json-patch'" but that gives compile warnings
+export type JSONPointer = string // Would like to use "import type { JSONPointer } from 'immutable-json-patch'" but that gives compile warnings
 export type JSONPointerMap<T> = { [pointer: JSONPointer]: T }
 
 export type ClipboardValues = Array<{ key: string; value: JSONValue }>
