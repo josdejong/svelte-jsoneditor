658c8aa6cc14f1d1b1307f2af9dadae33effdd34
Jos de Jong <wjosdejong@gmail.com>
2024-03-12 13:43:55 +0100

    chore: cleanup console.time for performance testing

--- src/lib/components/controls/SearchBox.svelte
+++ src/lib/components/controls/SearchBox.svelte
@@ -218,9 +218,7 @@
 
     setTimeout(() => {
       // wait until the search icon has been rendered
-      // console.time('search') // TODO: cleanup
       const newResultItems = search(searchText, json, { maxResults: MAX_SEARCH_RESULTS, columns })
-      // console.timeEnd('search') // TODO: cleanup
       searchResult = updateSearchResult(json, newResultItems, searchResult)
 
       searching = false
