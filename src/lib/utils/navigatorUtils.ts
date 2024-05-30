999acfc28b4c3bbb3475052373925bd3432ffc52
Jos de Jong <wjosdejong@gmail.com>
2024-05-30 16:26:06 +0200

    feat: upgrade dependencies `codemirror`, `ajv`, `jsonrepair`, and update dev dependencies

--- src/lib/utils/navigatorUtils.ts
+++ src/lib/utils/navigatorUtils.ts
@@ -1,3 +1,13 @@
+declare global {
+  // extend with the userAgentData: NavigatorUAData property (currently experimental)
+  // see https://developer.mozilla.org/en-US/docs/Web/API/Navigator/userAgentData
+  interface Navigator {
+    userAgentData?: {
+      platform: string
+    }
+  }
+}
+
 export const IS_MAC =
   navigator?.platform?.toUpperCase().includes('MAC') ??
   navigator?.userAgentData?.platform?.toUpperCase().includes('MAC') ??
