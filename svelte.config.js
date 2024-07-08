4120f2965ad96056e9e44d6587427afb1bd97fa3
Jos de Jong <wjosdejong@gmail.com>
2024-07-08 11:40:53 +0200

    chore: add a TODO to fully migrate to Svelte 5 runes

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-auto'
-import sveltePreprocess from 'svelte-preprocess'
+import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
   preprocess: sveltePreprocess(),
+
+  compilerOptions: {
+    // runes: true // TODO: fully migrate to runes mode
+  },
 
   kit: {
     adapter: adapter(),
