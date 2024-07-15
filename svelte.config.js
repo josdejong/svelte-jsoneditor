c4b823a7b6d69b42b795414e647685a441c983d2
Jos de Jong <wjosdejong@gmail.com>
2024-07-15 16:24:27 +0200

    chore: fix using a deprecated import of `svelte-preprocess`

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from '@sveltejs/adapter-auto'
-import sveltePreprocess from 'svelte-preprocess'
+import { sveltePreprocess } from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
