ad8c5735972db656fbe5dfb47dc35d57eb0c22d0
Jos de Jong <wjosdejong@gmail.com>
2024-10-14 14:20:30 +0200

    chore: cleanup Svelte workaround for Vitest tests

--- vitest.config.js
+++ vitest.config.js
@@ -4,10 +4,6 @@ import { sveltekit } from '@sveltejs/kit/vite'
 export default defineConfig({
   plugins: [sveltekit()],
   test: {
-    // workaround for a bug in svelte/vite/vitest introduced in vite@4.1.0
-    // see https://github.com/vitest-dev/vitest/issues/2834#issuecomment-1439576110
-    alias: [{ find: /^svelte$/, replacement: 'svelte/internal' }],
-
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     globals: true,
     environment: 'jsdom',
