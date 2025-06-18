d0460fe3cbb8aa1631ebc8c639ce6e9ed4958ab3
Jos de Jong <wjosdejong@gmail.com>
2025-06-18 14:31:35 +0200

    chore: add a unit test

--- src/lib/logic/actions.test.ts
+++ src/lib/logic/actions.test.ts
@@ -32,8 +32,9 @@ describe('actions', () => {
       expect(isMultilineText('"a":1,\n"b":2,')).toEqual(false)
     })
 
-    test('should return true when containing a newline', () => {
+    test('should return true when containing a newline and not being partial JSON', () => {
       expect(isMultilineText('A\nB\nC\n')).toEqual(true)
+      expect(isMultilineText('Hello,\nmulti line text')).toEqual(true)
     })
   })
 })
